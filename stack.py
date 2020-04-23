5f6744e24cb72eac1e836761d9ab2b66291b0e38
noirvin <vin.sp97@googlemail.com>
2020-04-22 21:36:21 -0700

    stack tests pass

---
+++ stack.py
@@ -0,0 +1,109 @@
+#!python
+
+from linkedlist import LinkedList
+
+
+# Implement LinkedStack below, then change the assignment at the bottom
+# to use this Stack implementation to verify it passes all tests
+class LinkedStack(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this stack and push the given items, if any."""
+        # Initialize a new linked list to store the items
+        self.list = LinkedList()
+        if iterable is not None:
+            for item in iterable:
+                self.push(item)
+
+    def __repr__(self):
+        """Return a string representation of this stack."""
+        return 'Stack({} items, top={})'.format(self.length(), self.peek())
+
+    def is_empty(self):
+        """Return True if this stack is empty, or False otherwise."""
+        # TODO: Check if empty
+        if self.list.is_empty():
+            return True
+        else:
+            return False
+
+    def length(self):
+        """Return the number of items in this stack."""
+        # TODO: Count number of items
+        return self.list.length()
+
+    def push(self, item):
+        """Insert the given item on the top of this stack.
+        Running time: O(???) – Why? [TODO]"""
+        # TODO: Push given item
+        self.list.append(item)
+    def peek(self):
+        """Return the item on the top of this stack without removing it,
+        or None if this stack is empty."""
+        # TODO: Return top item, if any
+        if self.is_empty():
+            return None
+        else:
+            return self.list.get_at_index(self.length()-1)
+    def pop(self):
+        """Remove and return the item on the top of this stack,
+        or raise ValueError if this stack is empty.
+        Running time: O(???) – Why? [TODO]"""
+        # TODO: Remove and return top item, if any
+        if self.list.is_empty():
+            raise ValueError("not found")
+        else:
+            top_item = self.peek()
+            self.list.delete(self.peek())
+            return top_item
+# Implement ArrayStack below, then change the assignment at the bottom
+# to use this Stack implementation to verify it passes all tests
+class ArrayStack(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this stack and push the given items, if any."""
+        # Initialize a new list (dynamic array) to store the items
+        self.list = list()
+        if iterable is not None:
+            for item in iterable:
+                self.push(item)
+
+    def __repr__(self):
+        """Return a string representation of this stack."""
+        return 'Stack({} items, top={})'.format(self.length(), self.peek())
+
+    def is_empty(self):
+        """Return True if this stack is empty, or False otherwise."""
+        # TODO: Check if empty
+        if self.length() == 0:
+            return True
+        else:
+            return False
+    def length(self):
+        """Return the number of items in this stack."""
+        # TODO: Count number of items
+        return len(self.list)
+
+    def push(self, item):
+        """Insert the given item on the top of this stack.
+        Running time: O(???) – Why? [TODO]"""
+        # TODO: Insert given item
+        self.list.append(item)
+
+    def peek(self):
+        """Return the item on the top of this stack without removing it,
+        or None if this stack is empty."""
+        # TODO: Return top item, if any
+        return self.list[self.length()-1]
+    def pop(self):
+        """Remove and return the item on the top of this stack,
+        or raise ValueError if this stack is empty.
+        Running time: O(???) – Why? [TODO]"""
+        # TODO: Remove and return top item, if any
+        self.list.pop()
+
+
+# Implement LinkedStack and ArrayStack above, then change the assignment below
+# to use each of your Stack implementations to verify they each pass all tests
+Stack = LinkedStack
+# Stack = ArrayStack
